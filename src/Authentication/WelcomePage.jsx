f5d7edfd78635c40eb49da2f52aaaa7a13bba633
vno-thvishnu <vinothvishnu12@gmail.com>
2023-11-22 12:49:19 +0530

    update

--- src/Authentication/WelcomePage.jsx
+++ src/Authentication/WelcomePage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./WelcomePage.css";
 // import Logo from "../img/logo2.png";
 import { Outlet } from "react-router-dom";
+import pic from "../Assets/welcomebg.png"
 
 const WelcomePage = () => {
   // const [isSignUp, setIsSignUp] = useState(false);
@@ -41,13 +42,17 @@ const WelcomePage = () => {
     <div className="Auth">
       <div className="a-left">
         {/* <img src={Logo} /> */}
+        <div className="form-container">
+        <Outlet />
+      </div>
         <div className="Webname">
           <h1>Hello Chef's</h1>
           <h6>You will become a chef</h6>
         </div>
       </div>
-      <div className="form-container">
-        <Outlet />
+     
+      <div>
+        <img src={pic}/>
       </div>
     </div>
   );
